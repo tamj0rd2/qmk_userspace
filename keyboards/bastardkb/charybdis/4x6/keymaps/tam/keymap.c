b06fbce2d6203e8bf1c2925e36726300c138ffc1
tamj0rd2 <20561445+tamj0rd2@users.noreply.github.com>
2025-08-16 12:43:46 +0100

    un-modtap keys for gaming

--- keyboards/bastardkb/charybdis/4x6/keymaps/tam/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/tam/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_B,    KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,       KC_N,    KC_M, UK_COMM,  UK_DOT, UK_SLSH, TG(2),
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,
+                                   KC_BSPC, KC_SPC, KC_ENT,      KC_TRNS,  KC_SPC,
                                            KC_TRNS, KC_TRNS,     KC_TRNS
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),

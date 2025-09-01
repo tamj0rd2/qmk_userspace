bf078587a143616bdf4194936d478a7138008cf5
tamj0rd2 <20561445+tamj0rd2@users.noreply.github.com>
2025-09-01 22:46:50 +0100

    similar setup to charybdis

---
+++ keyboards/mechboards/lily58/pro/keymaps/tam/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef VIA_ENABLE
+/* VIA configuration. */
+#    define DYNAMIC_KEYMAP_LAYER_COUNT 3
+#endif // VIA_ENABLE
+
+#define TAPPING_TERM 180
+#define CHORDAL_HOLD
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
